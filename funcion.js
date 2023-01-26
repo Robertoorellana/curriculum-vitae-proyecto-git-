a4d815e2ce6d2f0c7df61ab4d8dc70b1ec6ad931
Roberto Orellana <robertoorellana516@gmail.com>
2023-01-26 01:55:08 -0300

    primer escritura en el repositorio

---
+++ funcion.js
@@ -0,0 +1,25 @@
+document.getElementById('boton').onclick=function (){
+    console.log("capturmaos un evento click");
+document.getElementById("demo").innerHTML=".facebook.com/roberto orellana"
+}
+document.getElementById('boton1').onclick=function (){
+    console.log("capturmaos un evento click");
+document.getElementById("demo1").innerHTML=".instagram/ manu.art.r"
+}
+/*
+document.getElementById('boton1').onclick = function () {
+    document.getElementById('id1').style.display="block";
+    document.getElementById('id2').style.display ="block";
+    document.getElementById('id3').style.display ="block";
+    document.getElementById('id4').style.display ="block";
+    
+}
+   
+document.getElementById('id4').onclick = function () {
+    document.getElementById('id1').style.display ="none";
+    document.getElementById('id2').style.display ="none";
+    document.getElementById('id3').style.display ="none";
+    document.getElementById('id4').style.display ="none";
+}
+*/
+
---
+++ js function.js
@@ -0,0 +1,8 @@
+document.getElementById('boton').onclick=function (){
+    console.log("capturmaos un evento click");
+document.getElementById("demo").innerHTML="facebook.com/roberto orellana"
+}
+document.getElementById('boton1').onclick=function (){
+    console.log("capturmaos un evento click");
+document.getElementById("demo").innerHTML="instagram/ manu.art.r"
+}
