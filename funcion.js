e8ef9309cf7477fa93ebd69c377aaf6671b6a473
Roberto Orellana <robertoorellana516@gmail.com>
2023-01-27 02:56:05 -0300

    first commit

--- funcion.js
+++ funcion.js
@@ -6,20 +6,3 @@ document.getElementById('boton1').onclick=function (){
     console.log("capturmaos un evento click");
 document.getElementById("demo1").innerHTML=".instagram/ manu.art.r"
 }
-/*
-document.getElementById('boton1').onclick = function () {
-    document.getElementById('id1').style.display="block";
-    document.getElementById('id2').style.display ="block";
-    document.getElementById('id3').style.display ="block";
-    document.getElementById('id4').style.display ="block";
-    
-}
-   
-document.getElementById('id4').onclick = function () {
-    document.getElementById('id1').style.display ="none";
-    document.getElementById('id2').style.display ="none";
-    document.getElementById('id3').style.display ="none";
-    document.getElementById('id4').style.display ="none";
-}
-*/
-
